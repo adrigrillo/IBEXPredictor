a21c56f68ce1e9559d520ca5b48c07544a535b5e
Adrian Rodriguez Grillo <adrirgrillo@gmail.com>
2016-12-05 02:34:04 +0100

    Datos de todas las bolsas subidos

--- src/dataGetter.py
+++ src/dataGetter.py
@@ -3,20 +3,89 @@ quandl.ApiConfig.api_key = 'zxkqftXuQ9RAbjWsw_E2'
 
 
 def getIBEX35(type):
-    if type == 'normal':
+    if type == 'normal' or type == 'all':
         data = quandl.get('YAHOO/INDEX_IBEX', start_date='2005-01-01', collapse='daily')
         data.to_csv(path_or_buf='../data/Ibex35Normal', sep=',')
-    elif type == 'change':
+    if type == 'change' or type == 'all':
         data = quandl.get('YAHOO/INDEX_IBEX', start_date='2005-01-01', collapse='daily', transform="diff")
         data.to_csv(path_or_buf='../data/Ibex35Change', sep=',')
-    elif type == 'rchange':
+    if type == 'rchange' or type == 'all':
         data = quandl.get('YAHOO/INDEX_IBEX', start_date='2005-01-01', collapse='daily', transform="rdiff")
-        data.to_csv(path_or_buf='../data/Ibex35Change', sep=',')
+        data.to_csv(path_or_buf='../data/Ibex35RChange', sep=',')
+    print('Ibex35 done')
 
+
+def getDowJones(type):
+    if type == 'normal' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_DJI', start_date='2005-01-01', collapse='daily')
+        data.to_csv(path_or_buf='../data/DowJonesNormal', sep=',')
+    if type == 'change' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_DJI', start_date='2005-01-01', collapse='daily', transform="diff")
+        data.to_csv(path_or_buf='../data/DowJonesChange', sep=',')
+    if type == 'rchange' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_DJI', start_date='2005-01-01', collapse='daily', transform="rdiff")
+        data.to_csv(path_or_buf='../data/DowJonesRChange', sep=',')
+    print('Dow Jones done')
+
+
+def getNikkei(type):
+    if type == 'normal' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_N225', start_date='2005-01-01', collapse='daily')
+        data.to_csv(path_or_buf='../data/NikkeiNormal', sep=',')
+    if type == 'change' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_N225', start_date='2005-01-01', collapse='daily', transform="diff")
+        data.to_csv(path_or_buf='../data/NikkeiChange', sep=',')
+    if type == 'rchange' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_N225', start_date='2005-01-01', collapse='daily', transform="rdiff")
+        data.to_csv(path_or_buf='../data/NikkeiRChange', sep=',')
+    print('Nikkei done')
+
+
+def getEuroStock(type):
+    if type == 'normal' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_STOXX50E', start_date='2005-01-01', collapse='daily')
+        data.to_csv(path_or_buf='../data/EuroStockNormal', sep=',')
+    if type == 'change' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_STOXX50E', start_date='2005-01-01', collapse='daily', transform="diff")
+        data.to_csv(path_or_buf='../data/EuroStockChange', sep=',')
+    if type == 'rchange' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_STOXX50E', start_date='2005-01-01', collapse='daily', transform="rdiff")
+        data.to_csv(path_or_buf='../data/EuroStockRChange', sep=',')
+    print('LondonStock done')
+
+
+def getGermanyStock(type):
+    if type == 'normal' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_GDAXI', start_date='2005-01-01', collapse='daily')
+        data.to_csv(path_or_buf='../data/GermanyStockNormal', sep=',')
+    if type == 'change' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_GDAXI', start_date='2005-01-01', collapse='daily', transform="diff")
+        data.to_csv(path_or_buf='../data/GermanyStockChange', sep=',')
+    if type == 'rchange' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_GDAXI', start_date='2005-01-01', collapse='daily', transform="rdiff")
+        data.to_csv(path_or_buf='../data/GermanyStockRChange', sep=',')
+    print('GermanyStock done')
+
+
+def getFranceStock(type):
+    if type == 'normal' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_FCHI', start_date='2005-01-01', collapse='daily')
+        data.to_csv(path_or_buf='../data/FranceStockNormal', sep=',')
+    if type == 'change' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_FCHI', start_date='2005-01-01', collapse='daily', transform="diff")
+        data.to_csv(path_or_buf='../data/FranceStockChange', sep=',')
+    if type == 'rchange' or type == 'all':
+        data = quandl.get('YAHOO/INDEX_FCHI', start_date='2005-01-01', collapse='daily', transform="rdiff")
+        data.to_csv(path_or_buf='../data/FranceStockRChange', sep=',')
+    print('GermanyStock done')
 
 def main():
-    getIBEX35('normal')
-
+    getIBEX35('all')
+    getDowJones('all')
+    getNikkei('all')
+    getEuroStock('all')
+    getGermanyStock('all')
+    getFranceStock('all')
 
 if __name__ == '__main__':
     main()
