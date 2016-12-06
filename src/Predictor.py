0798b48e342623a09c4fd269880e6399e001589a
Adrian Rodriguez Grillo <adrirgrillo@gmail.com>
2016-12-06 14:41:49 +0100

    LSTM a crear

--- src/Predictor.py
+++ src/Predictor.py
@@ -1,53 +1,64 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-import matplotlib.pylab as plt
-from matplotlib.pylab import rcParams
-from statsmodels.tsa.arima_model import ARIMA
+import math as mt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
-rcParams['figure.figsize'] = 15, 6
+# fix random seed for reproducibility
+np.random.seed(7)
 
-
-def readData():
+def loadData():
     """
     Metodo que lee los datos para posteriormente poder usarlos, cambia la fecha de
-    texto a datetime
-    :return:
+    texto a datetime.
+    :return: Datos con respecto al cambio del dia anterior y datos en valores
     """
     transFecha = lambda fecha: pd.datetime.strptime(fecha, '%Y-%m-%d')
-    data = pd.read_csv('../data/Ibex35Normal', parse_dates=[0], index_col=0, date_parser=transFecha, usecols=[0,6])
-    timeSerie = pd.DataFrame(data)     #Convierte la tabla en un dataframe para acceder mejor a los datos
-    plt.plot(timeSerie["2016"])
-    #plt.show()
-    return timeSerie["2016"]
+    dataReal = pd.read_csv('../data/Ibex35Normal', engine='python', parse_dates=[0], index_col=0,
+                            date_parser=transFecha, usecols=[0,6], skipfooter=3)
+    adjClose = pd.read_csv('../data/Ibex35RChange', usecols=[6], engine='python')
+    dataChange = adjClose.values
+    return dataChange, dataReal
 
 
-def comprobar_estacionalidad(timeSerie):
-
-    #Determing rolling statistics
-    rolmean = timeSerie.rolling(window=5, center=False).mean()
-    rolstd = timeSerie.rolling(window=5, center=False).std()
-
-    #Plot rolling statistics:
-    orig = plt.plot(timeSerie, color='blue',label='Original')
-    mean = plt.plot(rolmean, color='red', label='Rolling Mean')
-    std = plt.plot(rolstd, color='black', label = 'Rolling Std')
-    plt.legend(loc='best')
-    plt.title('Rolling Mean & Standard Deviation')
-    plt.show()
+def traning_test(datos):
+    """
+    Metodo que establecera los conjuntos de test y entrenamiento
+    :param datos: Datos de la bolsa
+    :return: conjuntos de entrenamiento y test
+    """
+    train_size = int(len(datos) * 0.7)
+    train, test = datos[0:train_size,:], datos[train_size:len(datos),:]
+    print("Conjunto de entrenamiento, longitud: ", len(train))
+    print("Conjunto de test, longitud: ",  len(test))
+    return train, test
 
 
-def models(ts):
-    ts_log = np.log(ts)
-    ts_log_diff = ts_log - ts_log.shift()
-    model = ARIMA(ts_log, order=(1, 0, 1))
-    results_AR = model.fit(disp=-1)
-    plt.plot(ts_log_diff)
-    plt.plot(results_AR.fittedvalues, color='red')
-    plt.show()
+# convert an array of values into a dataset matrix
+def crearDataset(dataset, vista_atras=1):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-vista_atras-1):
+        a = dataset[i:(i+vista_atras), 0]
+        dataX.append(a)
+        dataY.append(dataset[i + vista_atras, 0])
+    return np.array(dataX), np.array(dataY)
 
 
-serie = readData()
-#comprobar_estacionalidad(serie)
-models(serie)
+def main():
+    change, real = loadData()
+    entrenamiento, test = traning_test(change)
+    entrenamientoX, entrenamientoY = crearDataset(entrenamiento)
+    testX, testY = crearDataset(test)
+    # reshape input to be [samples, time steps, features]
+    entrenamientoX = np.reshape(entrenamientoX, (entrenamientoX.shape[0], 1, entrenamientoX.shape[1]))
+    testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+
+if __name__ == '__main__':
+    main()
