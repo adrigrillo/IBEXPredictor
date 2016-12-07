c83d58a9e16cacacc50741530be68cb3668afdd3
Adrian Rodriguez Grillo <adrirgrillo@gmail.com>
2016-12-07 15:33:24 +0100

    Datos bien y representacion grafica

--- src/plotter.py
+++ src/plotter.py
@@ -3,8 +3,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_data(data):
-    data.plot()
+def plot_data(data, title):
+    plot = data.plot(title=title)
+    plot.set_xlabel("Fecha")
+    plot.set_ylabel("Datos")
     plt.show()
 
 
@@ -16,7 +18,7 @@ def main():
     raiz = '../data/'
     fichero = 'ProcessedNormalized.csv'
     data = pd.read_csv(filepath_or_buffer=raiz+fichero, index_col='Date', parse_dates=True)
-    plot_data(data)
+    plot_data(data, "Evolución de las bolsas de 2005-2016")
 
 
 if __name__ == '__main__':
